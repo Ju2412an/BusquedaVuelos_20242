c563e9ec1b54e49dd2f7de957209945f43b37c56
Ju2412an <ju2412an@gmail.com>
2024-10-28 06:51:50 -0500

    config sonar 3

--- src/main/java/co/edu/udea/codefactory/sitas/repository/FlightRepository.java
+++ src/main/java/co/edu/udea/codefactory/sitas/repository/FlightRepository.java
@@ -1,18 +1,19 @@
 package co.edu.udea.codefactory.sitas.repository;
 
-import co.edu.udea.codefactory.sitas.model.Airport;
-import co.edu.udea.codefactory.sitas.model.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.udea.codefactory.sitas.model.Airport;
+import co.edu.udea.codefactory.sitas.model.Flight;
+
 @Repository
-public interface FlightRepository extends JpaRepository<Flight, Long> {
+public interface FlightRepository extends JpaRepository<Flight, UUID> { // Cambia Long a UUID
     List<Flight> findByDepartureDateEqualsAndDepartureAirportEqualsAndArrivalAirportEquals(LocalDate departureDate, Airport departureAirport, Airport arrivalAirport);
 
-    Optional<Flight> findById(UUID id);
+    Optional<Flight> findById(UUID id); // Esto es correcto
 }
